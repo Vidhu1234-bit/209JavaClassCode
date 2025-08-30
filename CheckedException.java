e9bfd396d2ac7dff3eaab15d364b28759fd2be42
Vidhu1234-bit <vidhusrivastava719@gmail.com>
2025-08-30 07:21:28 +0530

    Add files via upload

---
+++ CheckedException.java
@@ -0,0 +1,16 @@
+
+import javax.annotation.processing.FilerException;
+
+
+
+
+
+
+
+
+
+public class CheckedException {
+    public static void main(String[] args) {
+        FilerException
+    }
+}
